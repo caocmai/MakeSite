67e4b524cd8beba08f3287392df6749f4e3c34c7
Cao Mai <caomai@rocketmail.com>
2021-02-01 21:23:12 -0800

    Add abilty to print content in text file

--- makesite.go
+++ makesite.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+)
+
+type Text struct {
+	Content string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	FileContents, err := ioutil.ReadFile("first-post.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print(string(FileContents))
+
+	t := template.Must(template.New("template.tmpl").ParseFiles("new.html"))
+
+	fmt.Print(t)
+	text := Text{Content: string(FileContents)}
+	err = t.Execute(os.Stdout, text)
+	// err = t.ExecuteTemplate(wr, "fileName", FileContents)
+
+	// t := template.Must(template.New("template.tmpl").Parse("new.html"))
+	// err = t.Execute(os.Stdout, FileContents)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// bytesToWrite := []byte("new.html")
+	// err = ioutil.WriteFile("new.html", bytesToWrite, 0644)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
 }
