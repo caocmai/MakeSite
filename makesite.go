de67078135364f9f0911c0e3e737437947904344
Cao Mai <caomai@rocketmail.com>
2021-02-02 16:00:24 -0800

    Complete MVP to generate HTML file

--- makesite.go
+++ makesite.go
@@ -1,34 +1,55 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"text/template"
 )
 
-type Story struct {
+type Post struct {
 	Content string
 }
 
-func main() {
-	fileContents, err := ioutil.ReadFile("first-post.txt")
+func createHTMLFile(name string) {
+	fileContent, err := ioutil.ReadFile(name + ".txt")
 	if err != nil {
 		panic(err)
 	}
-	// fmt.Print(string(FileContents))
+	// fmt.Print(string(fileContent))
+
+	// Adding flag?
+	filePtr := flag.String(name, "noName", "Sets flag to the file")
+	flag.Parse()
+	fmt.Print(*filePtr)
 
 	t := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
 
-	// t, err := template.New("todos").Parse("You have a task named \"{{ .Content}}\"")
-
-	text := Story{Content: string(fileContents)}
-
-	f, err := os.Create("first-post.html")
-
+	text := Post{Content: string(fileContent)}
+	f, err := os.Create(name + ".html")
 	err = t.Execute(f, text)
 
 	if err != nil {
 		panic(err)
 	}
+}
+
+func dir() {
+	directory := "."
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+}
+
+func main() {
+	createHTMLFile("latest-post")
+	// dir()
 
 }
